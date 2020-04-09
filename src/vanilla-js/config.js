2d81e3fac3c35f16cdb373df31787035ad56e1dc
Keif Gonzalez <CmdrKerfy@users.noreply.github.com>
2020-04-09 09:44:35 -0400

    first attempt at fixing polygon display issue

--- src/vanilla-js/config.js
+++ src/vanilla-js/config.js
@@ -69,7 +69,7 @@ var config = {
         },
 
         {
-            id: 'Week-1',
+            id: 'Week1',
             title: 'WEEK 1 PROJECTION',
             image: 'https://raw.githubusercontent.com/CmdrKerfy/covid-hiv-story-map-hcw/master/src/vanilla-js/images/week1combo.png',
             description: 'In week one into an outbreak, our capacity will be already be stressed but still generally manageable.',
@@ -99,36 +99,36 @@ var config = {
         },
 
         {
-            id: 'Week-1N',
-            image: 
+            id: 'Lamu',
+            //image: 'https://raw.githubusercontent.com/CmdrKerfy/covid-hiv-story-map-hcw/master/src/vanilla-js/images/kenyabed.jpg',
             description: 'By week 1, total staff capacity in Isiolo and Lamu will likely be non-existent.',
             location: {
-                center: [39.21280, -0.23952],
-                zoom: 7.66,
+                center: [39.34064, -0.50332],
+                zoom: 6.90,
                 pitch: 0.00,
                 bearing: 0.00
             },
             onChapterEnter: [{
-                layer: 'Kenya-NONE',
+                layer: 'April 8th - Doc Week1',
                 opacity: 0.45
             },
             {
-                layer: 'April 8th - Nurse CURR',
+                layer: 'April 8th - Nurse Week1',
                 opacity: 0.45
             }
         ],
             onChapterExit: [{
-                layer: 'Kenya-NONE',
+                layer: 'April 8th - Doc Week1',
                 opacity: 0.
             },
             {
-                layer: 'April 8th - Nurse CURR',
+                layer: 'April 8th - Nurse Week1',
                 opacity: 0
             }]
         },
 
         {
-            id: 'Week-2',
+            id: 'Week2',
             title: 'WEEK 2 PROJECTION',
             image: 'https://raw.githubusercontent.com/CmdrKerfy/covid-hiv-story-map-hcw/master/src/vanilla-js/images/week2chart.png',
             description: 'The health facility compared to fake neighborhoods',
@@ -139,107 +139,22 @@ var config = {
                 bearing: 0.00
             },
             onChapterEnter: [{
-                layer: 'April 8th - Doc Week2',
+                layer: 'April 8th - Doc Week3',
                 opacity: 0.45
             },
             {
-                layer: 'April 8th - Nurse Week2',
+                layer: 'April 8th - Nurse Week3',
                 opacity: 0.45
             }
         ],
             onChapterExit: [{
-                layer: 'April 8th - Doc Week2',
+                layer: 'April 8th - Doc Week3',
                 opacity: 0.
             },
             {
-                layer: 'April 8th - Nurse Week2',
+                layer: 'April 8th - Nurse Week3',
                 opacity: 0
             }]
         },
-
-        {
-            id: 'HOS-Province',
-            title: 'Hospital Provincial Lunda Sul',
-            image: 'http://s2.reutersmedia.net/resources/r/?m=02&d=20181025&t=2&i=1318543827&r=LYNXNPEE9O16B&w=1200',
-            description: 'The main hospital within Saurimo, this location also includes the Centro Materno Infantil de Saurimo.',
-            location: {
-                center: [20.39212, -9.66001],
-                zoom: 17.00,
-                pitch: 51.00,
-                bearing: -56.80
-            },
-            onChapterEnter: [{
-                    layer: 'Saurimo Color Health',
-                    opacity: 0.65
-                },
-                { 
-                    layer: 'Saurimo Buildings',
-                    opacity: 0.45
-                },
-                {
-                    layer: 'S Health Labels',
-                    opacity: 0  
-                },
-                {
-                    layer: 'Saurimo Fake Treatment',
-                    opacity: 0.65
-                }
-            ],
-            onChapterExit: [{
-                    layer: 'Saurimo Color Health',
-                    opacity: 0
-                },
-                {
-                    layer: 'Saurimo Fake Treatment',
-                    opacity: 0
-                },
-                { 
-                    layer: 'Saurimo Buildings',
-                    opacity: 0
-                },
-            ]
-        },
-
-        {
-            id: 'HOS-Province-2',
-            title: 'Hospital Provincial Lunda Sul',
-            image: 'http://s2.reutersmedia.net/resources/r/?m=02&d=20181025&t=2&i=1318543827&r=LYNXNPEE9O16B&w=1200',
-            description: 'The main hospital within Saurimo, this location also includes the Centro Materno Infantil de Saurimo.',
-            location: {
-                center: [20.39212, -9.66001],
-                zoom: 17.00,
-                pitch: 51.00,
-                bearing: -56.80
-            },
-            onChapterEnter: [{
-                layer: 'Saurimo Color Health',
-                opacity: 0.65
-            }],
-            onChapterExit: [{
-                layer: 'Saurimo Color Health',
-                opacity: 0
-            }],
-        },
-
-        {
-            id: 'HOS-Mulombe',
-            title: 'Centro de Saude Mulombe',
-            image: 'https://live.staticflickr.com/1943/31203198868_01b23c0821_b.jpg',
-            description: 'A health facility on the outside of the city. This center services a KP population.',
-            location: {
-                center: [20.39629, -9.69189],
-                zoom: 16.68,
-                pitch: 30.00,
-                bearing: -16.32
-            },
-            onChapterEnter: [{
-                layer: 'Saurimo Color Health',
-                opacity: 0.65
-            }, ],
-            onChapterExit: [{
-                layer: 'Saurimo Color Health',
-                opacity: 0
-            }, ]
-        }
     ]
 };
